ad439bdf05fdc77fba51c86d7f64a787ac71732b
FuturamaGuy2017 <81769195+JavascriptCoder999@users.noreply.github.com>
2021-08-21 09:15:44 +0200

    Create CODE-EXPLANATION.sh

---
+++ CODE-EXPLANATION.sh
@@ -0,0 +1,69 @@
+> ![alt](https://avatars.githubusercontent.com/u/81769195?s=48&v=4 "my profile PiCtUrE")
+#Profile picture in a blockquote
+> 
+>> > ![alt](https://avatars.githubusercontent.com/u/81769195?s=48&v=4 "my profile PiCtUrE")
+#Profile picture in a nested blockquote
+>> > 
+>>> ![alt](https://avatars.githubusercontent.com/u/81769195?s=48&v=4 "my profile PiCtUrE")
+#profile picture in the same nested blockquote
+- 👋 Hi, I’m @JavascriptCoder999
+#Wave
+- 👀 I’m interested in coding scratch, html and javascript
+#what I'm interested in coding
+- 🌱 I’m currently learning c#
+#What I'm currently learning (c sharp)
+- 💞️ I’m looking to collaborate on game projects and other cool stuff
+#What I'm looking to collaborate on
+- 😀find me on --[**`Bitbucket`**](https://bitbucket.org/draggable-dragon_28/ 'Bit')
+#Find me on... (bold and highlighted) (connected link)                         (hover over text)
+  - 😀Find me on --[**`Docker Hub`** *`Lost access to account because I forgot my Docker ID`*](https://hub.docker.com/u/mousewheel7 'Dock') 
+#Find me on...   (bold and highlighted) (italics and highlighted)                              (connected link)                   (hover over text)
+~~dang~~
+#Strikethrough ”dang”
+- thank you for following me (***`@lastsandwich`, `@mikevenforgithub`, `@code598`, `@code44598`, `@code-e3`, `@caringtiger`, `liobrasil` and `mechoutayoub`***!)
+# (bullet point)(thanks for following in bold, italics and highlighted)            
+```ml
+#Write in Machine learning language for coloured text
+discord:
+#My discord
+mouseWheel77 #8083
+#(name)        (tag)
+ADD ME PLEASE
+#add me please (yellow)
+```
+#end machine learning text // code
+`i think bitcoins will change the world in time`
+#cryptocurrency message (highlighted)
+#double enter because of markdown
+~~why is this text in strikethrough?~~
+#idiotic meant-to-be-funny message (strikethrough)
+#double line break because of markdown
+```diff
+#opens a diff code file for coloured text
+-i also hate how markdown needs you to enter twice
+# message in red
++i don't like it
+#message in green
+!i think that is only for discord
+#message in orange (green again in discord)
+```
+'#triple backtick // backquote to end the diff code
+> i am now on blue belt in my coding course (stage 5 out of 9)
+#message in blockquote
+>> wow
+#message in nested blockquote
+```diff
+#opens a diff code file for coloured text
++spotify: ducko'bama_961
+'#
+```
+```bash
+#If you follow me i will follow you back
+```
+```asciidoc
+= wow dark blue =
+```
+<!---
+JavascriptCoder999/JavascriptCoder999 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
